57c3a10c145ab554232ce52602b1f80dbd0d72b2
tinhpham2004 <boydzpro3032004@gmail.com>
2024-05-26 08:51:42 +0700

    complete onboarding

--- app/src/main/java/com/loc/newsapp/presentation/common/NewsButton.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/NewsButton.kt
@@ -5,15 +5,18 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.loc.newsapp.ui.theme.WhiteGray
 
 @Composable
 fun NewsButton(text: String, onClick: () -> Unit) {
     Button(
-        onClick = { /*TODO*/ },
+        onClick = onClick,
         colors = ButtonDefaults.buttonColors(
             containerColor = MaterialTheme.colorScheme.primary,
             contentColor = Color.White
@@ -23,6 +26,17 @@ fun NewsButton(text: String, onClick: () -> Unit) {
         Text(
             text = text,
             style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold)
+        )
+    }
+}
+
+@Composable
+fun NewsTextButton(text: String, onClick: () -> Unit) {
+    TextButton(onClick = onClick) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = WhiteGray
         )
     }
 }
